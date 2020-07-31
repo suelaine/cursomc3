4613e44ca34d7370a6817250a89c004980537d8b
suelaine <suelaine.andrade@gmail.com>
2020-07-31 10:08:49 -0300

    Projeto Criado

---
+++ src/main/java/com/suelaine/cursomc2/Cursomc3Application.java
@@ -0,0 +1,13 @@
+package com.suelaine.cursomc2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cursomc3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cursomc3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/suelaine/cursomc2/Cursomc3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suelaine.cursomc2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cursomc3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
