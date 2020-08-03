6b69afd9ca7957e25d041a5c3a364a527f974575
suelaine <suelaine.andrade@gmail.com>
2020-08-03 11:12:08 -0300

    Populando as Categorias no banco pelo Spring

--- src/main/java/com/suelaine/cursomc2/Cursomc3Application.java
+++ src/main/java/com/suelaine/cursomc2/Cursomc3Application.java
@@ -1,13 +1,32 @@
 package com.suelaine.cursomc2;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Cursomc3Application {
+import com.suelaine.cursomc2.domain.Categoria;
+import com.suelaine.cursomc2.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class Cursomc3Application implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(Cursomc3Application.class, args);
 	}
+	
+
+	@Override
+	public void run(String... args) throws Exception {		
+		Categoria cat1 = new Categoria(null,"Informatica");
+		Categoria cat2 = new Categoria(null,"Escritório");
+		Categoria cat3 = new Categoria(null,"Artes");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
+	}	
 
 }
